849bdb8020fb5993a8b63c1bfd10ec8fd0a281d7
Luan <phamvuluan131@gmail.com>
2023-03-23 03:49:58 +0700

    update land

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -26,7 +26,7 @@ const Landing = ({ setSelectedPage }) => {
         ) : (
           <img
             alt="profile"
-            className="z-10 w-full max-w-[400px] md:max-w-[600px]"
+            className="z-10 w-full max-w-[200px] md:max-w-[400px]"
             src="assets/profile-image.png"
           />
         )}
