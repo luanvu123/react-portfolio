3f09a9d4b2e560c8e86c707810c9d43992dbb1fd
Luan <phamvuluan131@gmail.com>
2023-03-22 23:10:11 +0700

    update json

---
+++ src/scenes/Projects.jsx
@@ -0,0 +1,111 @@
+import LineGradient from "../components/LineGradient";
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const projectVariant = {
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: { opacity: 1, scale: 1 },
+};
+
+const Project = ({ title ,subtitle}) => {
+  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
+    bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+  const projectTitle = title.split(" ").join("-").toLowerCase();
+
+  return (
+    <motion.div variants={projectVariant} className="relative">
+      <div className={overlayStyles}>
+        <p className="text-2xl font-playfair">{title}</p>
+        <p className="mt-7">{subtitle}</p>
+      </div>
+      <img src={`../assets/${projectTitle}.png`} alt={projectTitle} />
+    </motion.div>
+  );
+};
+
+const Projects = () => {
+  return (
+    <section id="projects" className="pt-48 pb-48">
+      {/* HEADINGS */}
+      <motion.div
+        className="md:w-2/5 mx-auto text-center"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, y: -50 },
+          visible: { opacity: 1, y: 0 },
+        }}
+      >
+        <div>
+          <p className="font-playfair font-semibold text-4xl">
+            <span className="text-red">PRO</span>JECTS
+          </p>
+          <div className="flex justify-center mt-5">
+            <LineGradient width="w-2/3" />
+          </div>
+        </div>
+        <p className="mt-10 mb-10">
+         I participate in online courses and
+practical activities to improve my skills
+and knowledge in web programming.       
+        </p>
+      </motion.div>
+
+      {/* PROJECTS */}
+      <div className="flex justify-center">
+        <motion.div
+          className="sm:grid sm:grid-cols-3"
+          variants={container}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.2 }}
+        >
+          {/* ROW 1 */}
+          <div
+            className="flex justify-center text-center items-center p-10 bg-red
+              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+          >
+            BEAUTIFUL USER INTERFACES
+          </div>
+          <Project title="Project 1" subtitle="Web film project:Team leader.Used PHP and MySQL to develop the web
+application, experienced in designing the database
+and optimizing queries for better performance.
+Understands basic concepts of HTML, CSS, and
+JavaScript, using libraries and frameworks like
+Bootstrap and jQuery to speed up development.
+Experienced in creating an admin page to manage
+data, including user accounts, movies, episodes,
+and comments.https://github.com/luanvu123/webphim-tutorial."/>
+          <Project title="Project 2" subtitle="Web Chat Project (Node.js).Using Reactjs and Node.js to develop a real-time web chat application. Understanding the basic concepts of ReactJS, utilizing libraries and frameworks such as Redux and Socket.io to increase development speed. Experienced in creating features such as login, registration, creating chat rooms, chatting with other users, and chatting with AI. https://github.com/luanvu123/chat-app."/>
+
+          {/* ROW 2 */}
+          <Project title="Project 3" subtitle="Notes taking Application.Create a simple Notes taking Application using Nodejs, Express, MongoDB, and Passport. For templating: EJS and Bootstrap. https://github.com/luanvu123/Notes-NodeJs-CRUD-MongoDB-main.git"/>
+          {/* <Project title="Project 4" />
+          <Project title="Project 5" /> */}
+
+          {/* ROW 3 */}
+          {/* <Project title="Project 6" />
+          <Project title="Project 7" /> */}
+          <div
+            className="flex justify-center text-center items-center p-10 bg-blue
+              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+          >
+            SMOOTH USER EXPERIENCE
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
